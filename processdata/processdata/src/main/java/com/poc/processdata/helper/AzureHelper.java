5b3e47b872055518106144cfa7a1a544ddecdccc
CORP\Kamal199261 <kamal.b@exlservice.com>
2024-02-27 20:39:09 +0530

    New adls Entra ID change and updated the VM parameters with the latest fields

--- processdata/processdata/src/main/java/com/poc/processdata/helper/AzureHelper.java
+++ processdata/processdata/src/main/java/com/poc/processdata/helper/AzureHelper.java
@@ -1,14 +1,17 @@
 package com.poc.processdata.helper;
 
-import com.azure.storage.file.datalake.DataLakeFileClient;
-import com.azure.storage.file.datalake.DataLakeFileSystemClient;
-import com.azure.storage.file.datalake.DataLakeServiceClientBuilder;
+import com.azure.identity.ClientSecretCredential;
+import com.azure.identity.ClientSecretCredentialBuilder;
+import com.azure.storage.file.datalake.*;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Slf4j
 @Component
@@ -18,37 +21,53 @@ public class AzureHelper {
     @Value("${spring.azure.adls.containerName}")
     private String containerName;
 
-    @Value("${spring.azure.adls.connectionString}")
-    private String connectionString;
-
     @Value("${spring.azure.adls.filePath}")
     private String adlsFilePath;
 
     @Value("${spring.batch.file.result}")
     private String resultPath;
 
-    @Value("${spring.batch.file.filePath}")
-    private String filePath;
+    @Value("${spring.azure.adls.tenantId}")
+    private  String tenant_id;
+
+    @Value("${spring.azure.adls.clientId}")
+    private  String client_id;
+
+    @Value("${spring.azure.adls.clientSecret}")
+    private  String client_sec;
+
+    @Value("${spring.azure.adls.sasStringUrl}")
+    private  String sas_url;
+
 
     public void pushToADLS() {
-        DataLakeServiceClientBuilder serviceClientBuilder = new DataLakeServiceClientBuilder()
-                .connectionString(connectionString);
-        DataLakeFileSystemClient fileSystemClient = serviceClientBuilder
-                .buildClient()
-                .getFileSystemClient(containerName);
-        File folder = new File(resultPath);
-        File[] files = folder.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                String destinationFilePath = adlsFilePath + file.getName();
-                DataLakeFileClient fileClient = fileSystemClient.getFileClient(destinationFilePath);
-                fileClient.uploadFromFile(file.getPath(), true);
-                log.info("File uploaded successfully: " + file.getName());
-            }
-        } else {
-            log.info("No files found in the local folder.");
+        ClientSecretCredential clientSecretCredential = new ClientSecretCredentialBuilder()
+                .clientId(client_id)
+                .clientSecret(client_sec)
+                .tenantId(tenant_id)
+                .build();
+
+        // Create DataLakeServiceClient
+        DataLakeServiceClient serviceClient = new DataLakeServiceClientBuilder()
+                .credential(clientSecretCredential)
+                .endpoint(sas_url)
+                .buildClient();
+        DataLakeFileSystemClient fileSystemClient = serviceClient.getFileSystemClient(containerName);
+
+
+        // Upload files from local folder to ADLS Gen2
+        try {
+            Files.list(Paths.get(resultPath)).forEach(filePath -> {
+                String fileName = filePath.getFileName().toString();
+                DataLakeDirectoryClient directoryClient = fileSystemClient.getDirectoryClient(String.valueOf(adlsFilePath));
+                DataLakeFileClient fileClient = directoryClient.createFile(fileName, true);
+                fileClient.uploadFromFile(filePath.toString(),true);
+                log.info("File uploaded successfully: " + fileName);
+                //System.out.println("Uploaded file: " + fileName);
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        log.info("File uploaded successfully to ADLS Gen2.");
     }
 
 }
